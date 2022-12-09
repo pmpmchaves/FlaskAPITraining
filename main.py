e42d5227447cc5efb765ee21dabb2771b2d41c52
pmpmchaves <pmpmchaves@gmail.com>
2022-12-09 22:59:05 +1100

    Changes to Main and test

--- main.py
+++ main.py
@@ -1,16 +1,22 @@
 #Personal note: when running this file on VSCode: <folder_path_to_conda_env>/python + <python_file_path>/<file_name>.py
 
-from flask import Flask
+from flask import Flask, request
 from flask_restful import Api, Resource
 
 app = Flask(__name__) #to start Flask app
 api = Api(app) #to initialize restful API
 
-class HelloWorld(Resource):
-    def get(self):
-        return {"data":"Hello World"}
+videos = {}
 
-api.add_resource(HelloWorld, "/helloworld")
+class Video(Resource):
+    def get(self,video_id):
+        return videos[video_id]
+
+    def put(self,video_id):
+        print(request.form)
+        return {}
+
+api.add_resource(Video, "/video/<int:video_id>")
 
 if __name__ == "__main__":
 #running debug=True because we are in a production env
--- test.py
+++ test.py
@@ -1,11 +1,10 @@
 #Personal note: when running this file on VSCode: <folder_path_to_conda_env>/python + <python_file_path>/<file_name>.py
 
 import requests
-from requests.auth import HTTPBasicAuth
 
 BASE = "http://127.0.0.1:5000/"
 
-response = requests.get(BASE + "helloworld")
+response = requests.get(BASE + "video/1", {"likes":10})
 
 print(response.status_code)
 print(response.json())
