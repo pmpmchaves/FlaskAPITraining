eaf785632b1a8cd27591e7e74e396ecee5136ae3
Pedro Chaves <pmpmchaves@gmail.com>
2022-12-11 00:40:54 +1100

    Additional changes, for loop no working

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ class VideoModel(db.Model):
     def __repr__(self):
         return f"Video(name={name}, views={views}, likes={likes})"
 
-#db.create_all() #to create the database, only do it once! That's why it was commented out...
+#db.create_all() #to create the database, only do it once! That's why it was commented out after being run one time...
 
 video_put_args = reqparse.RequestParser() #argument parser to define what the user MUST supply when adding a new video to the database
 video_put_args.add_argument("name", type=str, help= "Name of the video is required", required=True)
@@ -46,8 +46,10 @@ class Video(Resource):
     @marshal_with(resource_fields) #marshal_with takes the output from return and serializes it in a JSON format according to the resource_fields to make sure what we return is a serialized object and can be read by the get request
     def get(self, video_id):
         result = VideoModel.query.filter_by(id=video_id).first() #to search for the required video using its video_id on the VideoModel database. It searches and returns the first instance it finds.
+        
         if not result: #if there is no video_id in the database that matches our get request, we need to prevent the model from crashing
             abort(404, message = "Could not find video with that ID")
+        
         return result
 
     #method to submit video information
@@ -57,9 +59,11 @@ class Video(Resource):
         result = VideoModel.query.filter_by(id=video_id).first() #to check if the video_id we want to add/put to the database already exists. If it does, we need to make sure the code does not break
         if result: #if the video_id already exists, we will display an error message using abort method. This way, the code does not break
             abort(409, message ="Video ID taken...") 
+        
         video = VideoModel(id=video_id,name=args["name"],views=args["views"],likes=args["likes"]) #this creates a new instance of the VideoModel class, i.e. adds new information to our database
         db.session.add(video) #temporarily adding the created video into the database
         db.session.commit() #permanently adding the created video into the database. Similar to GIT, "video" does not go inside the commit command
+        
         return video, 201
 
     @marshal_with(resource_fields)
@@ -68,11 +72,13 @@ class Video(Resource):
         result = VideoModel.query.filter_by(id=video_id).first() #first we check if the video exists in the database
         if not result: #if the video_ID does not exist in the database, we abort
             abort(404, message = "Video does not exist, cannot update...")
+
         elements = [name, views, likes] 
         for element in elements:
-            if str(element) in args:
-                result.element = args[f"element"] #if the video does exist, we check which elements were requested to be changed in the database for that video_id and update that database instance with the new values
+            if args[f"{element}"]: #When we write if args[f"element"], we are testing if the element exists inside the video_update_args parser dictionary exists (meaning it is not None). Look at the IMPORTANT note written on the video_update_args parser in line 31 to understand better.
+                result.element = args[f"{element}"] #if the video does exist, we check which elements were requested to be changed in the database for that video_id and update that database instance with the new values
         db.session.commit() #No need to type db.session.add() since the changes are automatically temporarily updated in the database. However, we do need to commit those changes to make them permanent
+        
         return result
 
 
