df7521c8384069cd5360fbf58d1339de6d518954
Pedro Chaves <pmpmchaves@gmail.com>
2022-12-10 18:47:08 +1100

    Added database and made changes to main and test files

--- main.py
+++ main.py
@@ -1,10 +1,25 @@
 #Personal note: when running this file on VSCode: <folder_path_to_conda_env>/python + <python_file_path>/<file_name>.py
 
 from flask import Flask, request
-from flask_restful import Api, Resource, reqparse
+from flask_restful import Api, Resource, reqparse, abort
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__) #to start Flask app
 api = Api(app) #to initialize restful API
+app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///database.db' #define the location of our database
+db = SQLAlchemy(app) #to connect databse to app
+
+#Creating the database model (database columns and elements)
+class VideoModel(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    views = db.Column(db.Integer, nullable = False)
+    likes = db.Column(db.Integer, nullable = False)
+
+    def __repr__(self):
+        return f"Video(name={name}, views={views}, likes={likes})"
+
+#db.create_all() #to create the database, only do it once! That's why it was commented out...
 
 video_put_args = reqparse.RequestParser()
 video_put_args.add_argument("name", type=str, help= "Name of the video is required", required=True)
@@ -13,14 +28,35 @@ video_put_args.add_argument("likes", type=int, help= "Likes of the video is requ
 
 videos = {}
 
+#method to inform the user that video_id does not exist
+def abort_if_id_doesnt_exist(video_id):
+    if video_id not in videos:
+        abort(404, message = "Could not find video...")
+
+#method to inform the user that video_id already exists
+def abort_if_video_already_exists(video_id):
+    if video_id in videos:
+        abort(409, message = "The video ID you typed already exists...")
+
+
 class Video(Resource):
-    def get(self,video_id):
+    #method to get video information
+    def get(self, video_id):
+        abort_if_id_doesnt_exist(video_id)
         return videos[video_id]
 
-    def put(self,video_id):
+    #method to submit video information
+    def put(self, video_id):
+        abort_if_video_already_exists(video_id)
         args = video_put_args.parse_args()
         videos[video_id] = args
         return videos[video_id], 201
+
+    #method to delete an existing video
+    def delete(self, video_id):
+        abort_if_id_doesnt_exist(video_id)
+        del videos[video_id]
+        return "", 204
 
 api.add_resource(Video, "/video/<int:video_id>")
 
--- test.py
+++ test.py
@@ -4,8 +4,17 @@ import requests
 
 BASE = "http://127.0.0.1:5000/"
 
-response = requests.put(BASE + "video/1", {"likes":10, "names": "Peter", "views":10000})
-print(response.json())
+data = [{"likes":89, "name": "Carol", "views":4500},
+        {"likes":10, "name": "Manuel", "views":234000},
+        {"likes":1000, "name": "Johanna", "views":789000}
+        ]
+
+for i in range(len(data)):
+    response = requests.put(BASE + "video/" + str(i), data[i])
+    print(response.json())
+
+response = requests.delete(BASE + "video/0")
+print(response)
 input()
-response = requests.get(BASE + "video/1")
+response = requests.get(BASE + "video/2")
 print(response.json())
