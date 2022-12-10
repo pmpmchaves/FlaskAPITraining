663cc2f9e0cd3598a9b185d61e62de68a9f8138a
Pedro Chaves <pmpmchaves@gmail.com>
2022-12-10 14:20:00 +1100

    response fix

--- test.py
+++ test.py
@@ -4,7 +4,7 @@ import requests
 
 BASE = "http://127.0.0.1:5000/"
 
-response = requests.get(BASE + "video/1", {"likes":10})
+response = requests.put(BASE + "video/1", {"likes":10})
 
 print(response.status_code)
 print(response.json())
