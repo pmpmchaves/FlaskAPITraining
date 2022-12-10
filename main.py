fec54f0c11cf22b86711d76f8e57e34a821226ce
Pedro Chaves <pmpmchaves@gmail.com>
2022-12-10 22:35:12 +1100

    Updated database class and put function

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 #Personal note: when running this file on VSCode: <folder_path_to_conda_env>/python + <python_file_path>/<file_name>.py
 
-from flask import Flask, request
-from flask_restful import Api, Resource, reqparse, abort
+from flask import Flask
+from flask_restful import Api, Resource, reqparse, abort, fields, marshal_with
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__) #to start Flask app
@@ -26,35 +26,31 @@ video_put_args.add_argument("name", type=str, help= "Name of the video is requir
 video_put_args.add_argument("views", type=int, help= "Views of the video is required", required=True)
 video_put_args.add_argument("likes", type=int, help= "Likes of the video is required", required=True)
 
-videos = {}
-
-#method to inform the user that video_id does not exist
-def abort_if_id_doesnt_exist(video_id):
-    if video_id not in videos:
-        abort(404, message = "Could not find video...")
-
-#method to inform the user that video_id already exists
-def abort_if_video_already_exists(video_id):
-    if video_id in videos:
-        abort(409, message = "The video ID you typed already exists...")
-
+resource_fields = {
+                    "id":fields.Integer,
+                    "name":fields.String,
+                    "views":fields.Integer,
+                    "likes":fields.Integer
+                } #this dictionary defines the fields from the VideoModel that we want to return 
 
 class Video(Resource):
     #method to get video information
+    @marshal_with(resource_fields) #marshal_with takes the output from return and serializes it in a JSON format according to the resource_fields to make sure what we return is a serialized object and can be read by the get request
     def get(self, video_id):
-        abort_if_id_doesnt_exist(video_id)
-        return videos[video_id]
+        result = VideoModel.query.filter_by(id=video_id).first() #to search for the required video using its video_id on the VideoModel database. It searches and returns the first instance it finds.
+        return result
 
     #method to submit video information
+    @marshal_with(resource_fields) #to serialize response
     def put(self, video_id):
-        abort_if_video_already_exists(video_id)
-        args = video_put_args.parse_args()
-        videos[video_id] = args
-        return videos[video_id], 201
+        args = video_put_args.parse_args() #dictionary that stores all the values we submit (put request)
+        video = VideoModel(id=video_id,name=args["name"],views=args["views"],likes=args["likes"]) #this creates a new instance of the VideoModel class, i.e. adds new information to our database
+        db.session.add(video) #temporarily adding the created video into the database
+        db.session.commit() #permanently adding the created video into the database. Similar to GIT, "video" does not go inside the commit command
+        return video, 201
 
     #method to delete an existing video
     def delete(self, video_id):
-        abort_if_id_doesnt_exist(video_id)
         del videos[video_id]
         return "", 204
 
