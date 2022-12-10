37ac1a1c904d94e0e5b5c8b2d69e3c8b4aab3794
Pedro Chaves <pmpmchaves@gmail.com>
2022-12-10 18:02:19 +1100

    More changes

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 #Personal note: when running this file on VSCode: <folder_path_to_conda_env>/python + <python_file_path>/<file_name>.py
 
 from flask import Flask, request
-from flask_restful import Api, Resource
+from flask_restful import Api, Resource, reqparse
 
 app = Flask(__name__) #to start Flask app
 api = Api(app) #to initialize restful API
+
+video_put_args = reqparse.RequestParser()
+video_put_args.add_argument("name", type=str, help= "Name of the video is required", required=True)
+video_put_args.add_argument("views", type=int, help= "Views of the video is required", required=True)
+video_put_args.add_argument("likes", type=int, help= "Likes of the video is required", required=True)
 
 videos = {}
 
@@ -13,8 +18,9 @@ class Video(Resource):
         return videos[video_id]
 
     def put(self,video_id):
-        print(request.form)
-        return {}
+        args = video_put_args.parse_args()
+        videos[video_id] = args
+        return videos[video_id], 201
 
 api.add_resource(Video, "/video/<int:video_id>")
 
--- test.py
+++ test.py
@@ -4,7 +4,8 @@ import requests
 
 BASE = "http://127.0.0.1:5000/"
 
-response = requests.put(BASE + "video/1", {"likes":10})
-
-print(response.status_code)
+response = requests.put(BASE + "video/1", {"likes":10, "names": "Peter", "views":10000})
+print(response.json())
+input()
+response = requests.get(BASE + "video/1")
 print(response.json())
